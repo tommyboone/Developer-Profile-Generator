d686f3d785b4a6eb875dd58c3e93883c83463115
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-02-18 09:16:14 -0600

    style and layout updates to main-container

--- index.js
+++ index.js
@@ -17,14 +17,16 @@ const questions = [
   }
 ];
 
+
+
 function generate() {
   inquirer.prompt(questions).then(answers => {
     console.log(answers);
     const queryURL = `https://api.github.com/users/${answers.username}`;
     axios.get(queryURL).then(function(response) {
-    console.log(response.data)
+      console.log(response.data);
 
-    const newHTML = `<!DOCTYPE html>
+      const newHTML = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -36,6 +38,7 @@ function generate() {
         <link rel="stylesheet" type="text/css" href="style.css">
        
     </head>
+
     <body>
     <div class="jumbotron jumbotron-fluid">
     <div class="container main-container"> 
@@ -44,7 +47,9 @@ function generate() {
      <h1>Hi!</h1>
        <h2 class="display-4">My name is ${response.data.name}</h2>
      <p class = "bio">${response.data.bio}</p>
-      <p class = "info"><i class="fas fa-map-pin"></i> ${response.data.location}      
+      <p class = "info">
+      <a href="http://maps.google.com/maps?q=${response.data.location}">
+        <i class="fas fa-map-pin"></i> ${response.data.location}</a>   
       <a href ="${response.data.html_url}"><i class="fab fa-github"></i> GitHub    </a>        
       <a href ="${response.data.blog}"> <i class="fas fa-rss"></i> Portfolio </a>       
       
@@ -55,18 +60,24 @@ function generate() {
  
  <div class= "container content-container">
  <div class = "row">
+ <div class ="col-md-6 first-col">
       <div class="repos-container">
       
       <p class = "public-repos"> GitHub Repos: ${response.data.public_repos}</p>
       </div>
+      <br>
      <div class="followers-container">
       <p class= "followers"> Followers: ${response.data.followers}</p>
       </div>
+      </div>
+      <div class = "col-md-6 second-col">
       <div class="following-container">
       <p class= "following"> Following: ${response.data.following}</p>
       </div>
+      <br>
       <div class= "stars-container">
       <p class = "stars"> GitHub Stars: ${response.data.public_gists}</p>
+      </div>
       </div>
  </div>     
 </div>
@@ -86,22 +97,20 @@ function generate() {
     <script src="https://kit.fontawesome.com/8cf68bdaad.js" crossorigin="anonymous"></script>
     
     </body>
-    </html>`
-    
-         fs.writeFile('index.html',newHTML, function(err){
-             if (err){
-                 throw(err)
-             }
-             else{
-                 console.log("sucess!")
-                 console.log(response.data.blog)
-             }
-         } )
+    </html>`;
+
+      fs.writeFile("index.html", newHTML, function(err) {
+        if (err) {
+          throw err;
+        } else {
+          console.log("success!");
+          
+        }
+      });
     });
- 
-
-
-  })
+  });
 }
-{/* <p class="favColor">My favorite color is  ${answers.favColor}</p> */}
-generate()
+{
+
+}
+generate();
