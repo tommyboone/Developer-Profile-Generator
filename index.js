3e73dfb1271629a2100217f1333946fa35cf5f99
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-02-18 16:25:42 -0600

    background and div colors now change on user selection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-// const jquery = require("jquery");
+const jquery = require("jquery");
+
 // const util = require("util");
 
 const questions = [
@@ -22,6 +23,37 @@ const questions = [
 function generate() {
   inquirer.prompt(questions).then(answers => {
     console.log(answers);
+    let containerClass = 'content-container';
+    let infoContainer = 'info-container'
+    let picContainer = 'profilepic'
+    
+    switch(answers.favColor){
+        case 'blue':
+            containerClass = 'content-container-blue'
+            infoContainer = 'info-container-blue'
+            picContainer = 'profilepic-blue'
+            break;
+        case 'red':
+            containerClass= 'content-container-red'
+            infoContainer= 'info-container-red'
+            picContainer= 'profilepic-red'
+            break;
+        case 'yellow':
+            containerClass = 'content-container-yellow'
+            infoContainer= 'info-container-yellow'
+            picContainer= 'profilepic-yellow'
+            break;
+        case 'green':
+            containerClass= 'content-container-green'
+            infoContainer= 'info-container-green'
+            picContainer = 'profilepic-green'
+            break;
+        case 'pink':
+            containerClass= 'content-container-pink'
+            infoContainer= 'info-container-pink'
+            picContainer= 'profilepic-pink'
+            break;        
+    }
     const queryURL = `https://api.github.com/users/${answers.username}`;
     axios.get(queryURL).then(function(response) {
       console.log(response.data);
@@ -36,14 +68,15 @@ function generate() {
         <link href="https://fonts.googleapis.com/css?family=Rajdhani&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="style.css">
+
        
     </head>
 
     <body>
     <div class="jumbotron jumbotron-fluid">
-    <div class="container main-container"> 
+    <div class="container main-container ${containerClass}"> 
     
-     <a href="${response.data.blog}"><img id="profilepic" src = ${response.data.avatar_url}/></a>
+     <a href="${response.data.blog}"><img id="${picContainer}" src = ${response.data.avatar_url}/></a>
      <h1>Hi!</h1>
        <h2 class="display-4">My name is ${response.data.name}</h2>
      <p class = "bio">${response.data.bio}</p>
@@ -58,24 +91,24 @@ function generate() {
   </div> 
  </div>
  
- <div class= "container content-container">
+ <div class= "container ${containerClass}">
  <div class = "row">
  <div class ="col-md-6 first-col">
-      <div class="repos-container">
+      <div class="repos-container ${infoContainer}">
       
       <p class = "public-repos"> GitHub Repos: ${response.data.public_repos}</p>
       </div>
       <br>
-     <div class="followers-container">
+     <div class="followers-container ${infoContainer}">
       <p class= "followers"> Followers: ${response.data.followers}</p>
       </div>
       </div>
-      <div class = "col-md-6 second-col">
-      <div class="following-container">
+      <div class = "col-md-6 second-col ">
+      <div class="following-container ${infoContainer}">
       <p class= "following"> Following: ${response.data.following}</p>
       </div>
       <br>
-      <div class= "stars-container">
+      <div class= "stars-container ${infoContainer}">
       <p class = "stars"> GitHub Stars: ${response.data.public_gists}</p>
       </div>
       </div>
@@ -102,17 +135,13 @@ function generate() {
       fs.writeFile("index.html", newHTML, function(err) {
         if (err) {
           throw err;
-          
-        } 
-        else {
+        } else {
           console.log("success!");
-          
         }
       });
     });
   });
 }
 {
-
 }
 generate();
